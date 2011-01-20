63ef580ca047f20d4c8b3992438da33bde8245f8
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-01-20 13:41:33 +0000

    Finish of adj_create_variable

--- src/adj_data_structures.c
+++ src/adj_data_structures.c
@@ -5,12 +5,20 @@ int adj_create_variable(char* name, int timestep, int iteration, int auxiliary, 
 {
   size_t slen;
 
+  /* zero any struct padding bytes; we'll be hashing this later */
+  memset(var, 0, sizeof(adj_variable));
+
   slen = strlen(name);
   if (slen > ADJ_NAMELEN)
   {
     strncpy(adj_error_msg, "Name variable too long; recompile with bigger ADJ_NAMELEN", ADJ_ERROR_MSG_BUF);
     return ADJ_ERR_INVALID_INPUTS;
   }
+
+  strncpy(var->name, name, ADJ_NAMELEN);
+  var->timestep = timestep;
+  var->iteration = iteration;
+  var->auxiliary = auxiliary;
 
   return ADJ_ERR_OK;
 }
