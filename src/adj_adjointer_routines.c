8d99a771483d6b110509ee2f30884ebac084337a
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-01-24 20:08:26 +0000

    Temporary commit so I can merge

--- src/adj_adjointer_routines.c
+++ src/adj_adjointer_routines.c
@@ -5,9 +5,73 @@ int adj_destroy_adjointer(adj_adjointer* adjointer);
 int adj_set_option(adj_adjointer* adjointer, int option, int choice);
 int adj_equation_count(adj_adjointer* adjointer, int* count);
 int adj_register_equation(adj_adjointer* adjointer, adj_variable var, int nblocks, adj_block* blocks, adj_variable* targets, int nrhsdeps, adj_variable* rhsdeps);
-int adj_record_variable(adj_adjointer* adjointer, adj_variable var, adj_vector value);
-int adj_record_auxiliary(adj_adjointer* adjointer, adj_variable var, adj_vector value);
-int adj_register_operator_callback(adj_adjointer* adjointer, int type, char* name, void (*fn)(void)); */
+int adj_record_variable(adj_adjointer* adjointer, adj_variable var, adj_vector value); */
+
+int adj_record_auxiliary(adj_adjointer* adjointer, adj_variable var, adj_vector value)
+{
+  if (adjointer->options[ADJ_ACTIVITY] == ADJ_ACTIVITY_NOTHING) return ADJ_ERR_OK;
+}
+
+int adj_register_operator_callback(adj_adjointer* adjointer, int type, char* name, void (*fn)(void))
+{
+  adj_op_callback_list* cb_list_ptr;
+  adj_op_callback* cb_ptr;
+
+  switch(type)
+  {
+    case ADJ_NBLOCK_COLOURING_CB:
+      cb_list_ptr = &(adjointer->nonlinear_colouring_list);
+      break;
+    case ADJ_NBLOCK_ACTION_CB:
+      cb_list_ptr = &(adjointer->nonlinear_action_list);
+      break;
+    case ADJ_NBLOCK_DERIVATIVE_ACTION_CB:
+      cb_list_ptr = &(adjointer->nonlinear_derivative_action_list);
+      break;
+    case ADJ_NBLOCK_DERIVATIVE_ASSEMBLY_CB:
+      cb_list_ptr = &(adjointer->nonlinear_derivative_assembly_list);
+      break;
+    case ADJ_BLOCK_ACTION_CB:
+      cb_list_ptr = &(adjointer->block_action_list);
+      break;
+    case ADJ_BLOCK_ASSEMBLY_CB:
+      cb_list_ptr = &(adjointer->block_assembly_list);
+      break;
+    default:
+      strncpy(adj_error_msg, "Unknown callback type.", ADJ_ERROR_MSG_BUF);
+      return ADJ_ERR_INVALID_INPUTS;
+  }
+  /* First, we look for an existing callback data structure that might already exist, to replace the function */
+  cb_ptr = cb_list_ptr->firstnode;
+  while (cb_ptr != NULL)
+  {
+    if (strncmp(cb_ptr->name, name, ADJ_NAMELEN) == 0)
+    {
+      cb_ptr->callback = fn;
+      return ADJ_ERR_OK;
+    }
+    cb_ptr = cb_ptr->next;
+  }
+
+  /* If we got here, that means that we didn't find it. Tack it on to the end of the list. */
+  cb_ptr = (adj_op_callback*) malloc(sizeof(adj_op_callback));
+  strncpy(cb_ptr->name, name, ADJ_NAMELEN);
+  cb_ptr->callback = fn;
+
+  /* Special case for the first callback */
+  if (cb_list_ptr->firstnode == NULL)
+  {
+    cb_list_ptr->firstnode = cb_ptr;
+    cb_list_ptr->lastnode = cb_ptr;
+  }
+  else
+  {
+    cb_list_ptr->lastnode->next = cb_ptr;
+    cb_list_ptr->lastnode = cb_ptr;
+  }
+
+  return ADJ_ERR_OK;
+}
 
 int adj_register_data_callback(adj_adjointer* adjointer, int type, void (*fn)(void))
 {
