4dd1b304163dcc15c5cbef313c1790d186350f89
Simon Funke <simon.funke@gmail.com>
2011-01-27 12:55:35 +0000

    new (emtpy) test src/tests/test_adj_forget_equation.c

---
+++ src/tests/test_adj_forget_equation.c
@@ -0,0 +1,61 @@
+#include "adj_adjointer_routines.h"
+#include "adj_data_structures.h"
+#include "adj_test_tools.h"
+
+void test_adj_forget_equation()
+{
+  int ierr, cnt;
+  adj_adjointer adjointer;
+  adj_create_adjointer(&adjointer);
+  adj_variable vars[5];
+  adj_block blocks[5];
+  adj_equation equation;
+
+  adj_create_variable("Velocity", 0, 0, ADJ_NORMAL_VARIABLE, &vars[0]);
+  adj_create_variable("Velocity", 1, 0, ADJ_NORMAL_VARIABLE, &vars[1]);
+  adj_create_variable("Velocity", 1, 1, ADJ_NORMAL_VARIABLE, &vars[2]);
+  adj_create_variable("Velocity", 2, 0, ADJ_NORMAL_VARIABLE, &vars[3]);
+  adj_create_variable("Velocity", 1, 1, ADJ_NORMAL_VARIABLE, &vars[4]);
+
+  adj_create_block("IdentityOperator", NULL, NULL, 0, &blocks[0]);
+  ierr=adj_create_equation(vars[0], 1, &blocks[0], &vars[0], &equation);
+  adj_test_assert(ierr==ADJ_ERR_OK, "Should work");
+  adj_register_equation(&adjointer, equation);
+  adj_test_assert(ierr==ADJ_ERR_OK, "Should work");
+  ierr=adj_destroy_equation(&equation);
+  adj_test_assert(ierr==ADJ_ERR_OK, "Should work");
+
+  adj_create_block("TimesteppingOperator", NULL, NULL, 0, &blocks[1]);
+  adj_create_block("BurgersOperator", NULL, NULL, 0, &blocks[2]);
+
+  /*
+
+  ierr=adj_create_equation(vars[0], 1, &blocks[0], &vars[0], &equation); 
+  adj_test_assert(ierr==ADJ_ERR_OK, "Should work");
+  adj_register_equation(&adjointer, equation);
+  adj_test_assert(ierr==ADJ_ERR_OK, "Should work");
+
+  adj_equation_count(&adjointer, &cnt);
+  adj_test_assert(cnt == 1, "equation count");
+
+  ierr=adj_register_equation(&adjointer, equation);
+  adj_test_assert(ierr!= ADJ_ERR_OK, "Can't register again");
+  ierr=adj_destroy_equation(&equation);
+  adj_test_assert(ierr==ADJ_ERR_OK, "Should work");
+
+  adj_equation_count(&adjointer, &cnt);
+  adj_test_assert(cnt == 1, "Wrong equation count");
+
+  ierr=adj_create_equation(vars[1], 2, blocks, vars, &equation); 
+  adj_test_assert(ierr== ADJ_ERR_OK, "Should work");
+  ierr=adj_register_equation(&adjointer, equation);
+  adj_test_assert(ierr == ADJ_ERR_OK, "Register equation");
+  ierr=adj_destroy_equation(&equation);
+  adj_test_assert(ierr==ADJ_ERR_OK, "Should work");
+
+  adj_equation_count(&adjointer, &cnt);
+  adj_test_assert(cnt == 2, "Wrong equation count");
+
+  adj_destroy_adjointer(&adjointer); 
+  */
+}
