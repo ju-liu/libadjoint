b7251fc79f4c0969929d524b938d8315e29c09ba
Simon Funke <simon.funke@gmail.com>
2011-01-27 19:13:11 +0000

    creates adj_evaluation.c/h and implements adj_evaluate_block_action also a little test is included

---
+++ src/adj_evaluation.c
@@ -0,0 +1,32 @@
+#include "libadjoint/adj_evaluation.h"
+
+int adj_evaluate_block_action(adj_adjointer* adjointer, adj_block block, adj_vector input, adj_vector* output)
+{
+  int i, ierr;
+  void (*block_action_func)(int, adj_variable*, adj_vector*, int, adj_vector, void*, adj_vector*)=NULL;
+  adj_vector* dependencies = NULL;
+  int nb_variables=0;
+  adj_variable* variables=NULL;
+
+  ierr = adj_find_operator_callback(adjointer, ADJ_BLOCK_ACTION_CB, block.name, (void (**)(void)) &block_action_func);
+  if (ierr!=0)
+    return ierr;
+
+  /* We have the right callback, so let's call it already */ 
+  if (block.has_nonlinear_block) 
+  { 
+    /* we need to set up the dependencies */
+    nb_variables = block.nonlinear_block.ndepends;
+    variables = block.nonlinear_block.depends;
+    dependencies = (adj_vector*) malloc(nb_variables * sizeof(adj_vector));
+
+    for (i = 0; i < nb_variables; i++)
+    {
+      ierr = adj_get_variable_value(adjointer, variables[i], &dependencies[i]);
+      if (ierr != 0)
+        return ierr;
+    }
+  }
+  block_action_func(nb_variables, variables, dependencies, block.hermitian, input, block.context, output );
+  return 0;
+}
