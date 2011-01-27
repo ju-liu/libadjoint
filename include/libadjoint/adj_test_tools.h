f31543579997a95e8fd055f8581020d4f1f54c38
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-01-27 19:40:29 +0000

    Silence another intel warning

--- include/libadjoint/adj_test_tools.h
+++ include/libadjoint/adj_test_tools.h
@@ -1,7 +1,7 @@
 #ifndef ADJ_TEST_TOOLS_H
 #define ADJ_TEST_TOOLS_H
 
-void adj_test_assert(unsigned char passed, char *testdesc);
+void adj_test_assert(int passed, char *testdesc);
 
 #endif
 
--- src/adj_test_tools.c
+++ src/adj_test_tools.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "libadjoint/adj_test_tools.h"
 
-void adj_test_assert(unsigned char passed, char *testdesc)
+void adj_test_assert(int passed, char *testdesc)
 {
   if (!passed) 
     printf("  fail: %s\n", testdesc);
