20f8b75eca5b37f2d5173c350a9c03a7ee47ce03
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-01-27 19:16:32 +0000

    adj_simplification stubs

---
+++ src/adj_simplification.c
@@ -0,0 +1,7 @@
+#include "libadjoint/adj_simplification.h"
+
+void adj_simplify_derivatives(adj_adjointer* adjointer, adj_nonlinear_block_derivative* input, adj_nonlinear_block_derivative** output);
+int adj_simplification_compare(adj_nonlinear_block_derivative d1, adj_nonlinear_block_derivative d2);
+void adj_simplficiation_merge(adj_adjointer* adjointer, adj_nonlinear_block_derivative* d1, adj_nonlinear_block_derivative d2, int merged);
+adj_nonlinear_block_derivative adj_create_nonlinear_block_derivative(adj_adjointer* adjointer, adj_nonlinear_block nblock, adj_variable var, adj_vector contraction, int hermitian);
+void adj_destroy_nonlinear_block_derivative(adj_adjointer*, adj_nonlinear_block_derivative* derivative);
