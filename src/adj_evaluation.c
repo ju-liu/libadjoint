b3446a4cc6f1309addf339a8c23866cafbd70fea
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-01-28 09:31:04 +0000

    Fix style and memory leak in adj_evaluation.c

--- src/adj_evaluation.c
+++ src/adj_evaluation.c
@@ -3,13 +3,13 @@
 int adj_evaluate_block_action(adj_adjointer* adjointer, adj_block block, adj_vector input, adj_vector* output)
 {
   int i, ierr;
-  void (*block_action_func)(int, adj_variable*, adj_vector*, int, adj_vector, void*, adj_vector*)=NULL;
+  void (*block_action_func)(int, adj_variable*, adj_vector*, int, adj_vector, void*, adj_vector*) = NULL;
   adj_vector* dependencies = NULL;
-  int nb_variables=0;
-  adj_variable* variables=NULL;
+  int nb_variables = 0;
+  adj_variable* variables = NULL;
 
   ierr = adj_find_operator_callback(adjointer, ADJ_BLOCK_ACTION_CB, block.name, (void (**)(void)) &block_action_func);
-  if (ierr!=0)
+  if (ierr != ADJ_ERR_OK)
     return ierr;
 
   /* We have the right callback, so let's call it already */ 
@@ -27,26 +27,31 @@ int adj_evaluate_block_action(adj_adjointer* adjointer, adj_block block, adj_vec
         return ierr;
     }
   }
+
   block_action_func(nb_variables, variables, dependencies, block.hermitian, input, block.context, output );
-  return 0;
+
+  if (block.has_nonlinear_block)
+    free(dependencies);
+
+  return ADJ_ERR_OK;
 }
 
 
 int adj_evaluate_block_assembly(adj_adjointer* adjointer, adj_block block, adj_matrix *output)
 {
   int i, ierr;
-  void (*block_assembly_func)(int, adj_variable*, adj_vector*, int, void*, adj_matrix*)=NULL;
+  void (*block_assembly_func)(int, adj_variable*, adj_vector*, int, void*, adj_matrix*) = NULL;
   adj_vector* dependencies = NULL;
-  int nb_variables=0;
-  adj_variable* variables=NULL;
+  int nb_variables = 0;
+  adj_variable* variables = NULL;
 
   ierr = adj_find_operator_callback(adjointer, ADJ_BLOCK_ASSEMBLY_CB, block. name, (void (**)(void)) &block_assembly_func);
-  if (ierr!=0)
+  if (ierr != ADJ_ERR_OK)
     return ierr;
 
   /* We have the right callback, so let's call it already */ 
-  if (block.has_nonlinear_block) 
-  { 
+  if (block.has_nonlinear_block)
+  {
     /* we need to set up the dependencies */
     nb_variables = block.nonlinear_block.ndepends;
     variables = block.nonlinear_block.depends;
@@ -59,8 +64,13 @@ int adj_evaluate_block_assembly(adj_adjointer* adjointer, adj_block block, adj_m
         return ierr;
     }
   }
+
   block_assembly_func(nb_variables, variables, dependencies, block.hermitian, block.context, output );
-  return 0;
+
+  if (block.has_nonlinear_block)
+    free(dependencies);
+
+  return ADJ_ERR_OK;
 }
 
 
