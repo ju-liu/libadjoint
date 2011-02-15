6ff0a17fb5fcf50c5b18a4b244a5e3344c902b51
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-02-15 22:51:11 +0000

    A few minor header fixes

--- include/libadjoint/adj_evaluation.h
+++ include/libadjoint/adj_evaluation.h
@@ -1,9 +1,9 @@
 #ifndef ADJ_EVALUATION_H
 #define ADJ_EVALUATION_H
 
-#include "libadjoint/adj_data_structures.h"
-#include "libadjoint/adj_error_handling.h"
-#include "libadjoint/adj_adjointer_routines.h"
+#include "adj_data_structures.h"
+#include "adj_error_handling.h"
+#include "adj_adjointer_routines.h"
 
 #ifndef ADJ_HIDE_FROM_USER
 int adj_evaluate_block_action(adj_adjointer* adjointer, adj_block block, adj_vector input, adj_vector* output);
