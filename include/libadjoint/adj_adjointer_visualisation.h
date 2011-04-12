0ed491e4403eafc5260d553bfd92b51e5ac5c257
Simon Funke <simon.funke@gmail.com>
2011-04-12 17:55:22 +0100

    show hermitian block information and highlight target blocks

--- include/libadjoint/adj_adjointer_visualisation.h
+++ include/libadjoint/adj_adjointer_visualisation.h
@@ -16,11 +16,11 @@ void adj_html_header(FILE *fp);
 void adj_html_footer(FILE *fp);
 void adj_html_table_begin(FILE* fp);
 void adj_html_table_end(FILE* fp);
-void adj_html_write_row(FILE* fp, char** strings, char** desc, int nb_strings);
+void adj_html_write_row(FILE* fp, char** strings, char** desc, int nb_strings, int diag_index);
 int adj_html_find_column_index(adj_adjointer* adjointer, adj_variable* variable, int* col);
 void adj_html_vars(FILE* fp, adj_adjointer* adjointer, int type);
-int adj_html_eqn(FILE* fp, adj_adjointer* adjointer, adj_equation adj_eqn);
-int adj_html_adjoint_eqn(FILE* fp, adj_adjointer* adjointer, adj_equation fwd_eqn);
+int adj_html_eqn(FILE* fp, adj_adjointer* adjointer, adj_equation adj_eqn, int diag_index);
+int adj_html_adjoint_eqn(FILE* fp, adj_adjointer* adjointer, adj_equation fwd_eqn, int diag_index);
 int adj_html_adjoint_system(adj_adjointer* adjointer, char* filename);
 int adj_html_forward_system(adj_adjointer* adjointer, char* filename);
 #endif
