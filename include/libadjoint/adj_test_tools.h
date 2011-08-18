81c049fa6e302cff7cf95ca21c200f9bcc25e3e6
Simon Funke <simon.funke@gmail.com>
2011-08-18 12:16:37 +0100

    a unit test that checks that the fortran and C datatypes are of the same size. Currently only the adjointer is checked.

--- include/libadjoint/adj_test_tools.h
+++ include/libadjoint/adj_test_tools.h
@@ -1,7 +1,9 @@
 #ifndef ADJ_TEST_TOOLS_H
 #define ADJ_TEST_TOOLS_H
+#include "libadjoint/adj_data_structures.h"
 
 void adj_test_assert(int passed, char *testdesc);
+int adj_sizeof_adjointer(void);
 
 #endif
 
