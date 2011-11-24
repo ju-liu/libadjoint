528d411b18f5c8c69e78f92b27de5339333c00e1
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-11-24 08:36:31 +0000

    Some more whitespacing.

--- python/libadjoint/libadjoint_numpy.py
+++ python/libadjoint/libadjoint_numpy.py
@@ -1,23 +1,15 @@
 import libadjoint
 import numpy
 
-
 class Vector(libadjoint.Vector):
-    def __init__(self, vec):
-        '''Vector(vec)
-        class to wrap the numpy vector vec in a libadjoint vector.'''
+  def __init__(self, vec):
+    '''Vector(vec)
+    class to wrap the numpy vector vec in a libadjoint vector.'''
+    self.vec=vec
 
-        self.vec=vec
+  def duplicate(self):
+    return Vector(numpy.zeros(self.vec.size))
 
-    def duplicate(self):
-        
-        return Vector(numpy.zeros(self.vec.size))
-
-    def axpy(self, alpha, x):
-        
-        self.vec += alpha*x.vec
-    
-    
-
-    
+  def axpy(self, alpha, x):
+    self.vec += alpha*x.vec
 
