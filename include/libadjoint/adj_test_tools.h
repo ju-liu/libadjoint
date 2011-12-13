86ce86b42e00b257688a2c0f9e02d08f69228dee
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2011-12-13 09:45:18 +0000

    Hmm, maybe this will fix Marie's problem.

--- include/libadjoint/adj_test_tools.h
+++ include/libadjoint/adj_test_tools.h
@@ -1,6 +1,6 @@
 #ifndef ADJ_TEST_TOOLS_H
 #define ADJ_TEST_TOOLS_H
-#include "libadjoint/adj_data_structures.h"
+#include "adj_data_structures.h"
 
 void adj_test_assert(int passed, char *testdesc);
 int adj_sizeof_adjointer(void);
