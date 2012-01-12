b1e953580414dcd761fa1995e6d6f19580b0030e
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-01-12 06:34:17 +0000

    Fix the numpy libadjoint-python test (bitrot had set in)

--- src/tests/test_libadjoint_numpy.py
+++ src/tests/test_libadjoint_numpy.py
@@ -15,26 +15,11 @@ def id_assemble(variables, dependencies, hermitian, coefficient, context):
 
 b.assemble=id_assemble
 
-def rhs_cb(adjointer, variable, dependencies, values, context):
-    return v
-
-e=libadjoint.Equation(var, [b], [var], rhs_cb=rhs_cb)
+e=libadjoint.Equation(var, [b], [var], rhs=RHS(v))
 
 A.register_equation(e)
 
 A.record_variable(var, libadjoint.MemoryStorage(v))
 
-var1=libadjoint.Variable('foo', 1)
-
-def rhs_cb1(adjointer, variable, dependencies, values, context):
-    return values[0]
- 
-e1=libadjoint.Equation(var1, [b], [var1], rhs_cb=rhs_cb1, rhs_deps=[var])
-
-A.register_equation(e1)
-
 (var, soln0) = A.get_forward_solution(0)
 libadjoint.adj_test_assert(all(soln0.vec[:] == v.vec[:]), "First solution should be v")
-
-(var, soln1) = A.get_forward_solution(1)
-libadjoint.adj_test_assert(all(soln0.vec[:] == soln1.vec[:]), "Second solution should also be v")
