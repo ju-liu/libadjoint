a2a0d21513ec4f7b8de695f341ae96be7b9d5395
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-03-06 13:02:23 +0000

    Initial SLEPc integration. Still haven't done anything about the matrix-free shell operators.

---
+++ include/libadjoint/adj_predictability.h
@@ -0,0 +1,28 @@
+#ifndef ADJ_PREDICTABILITY_H
+#define ADJ_PREDICTABILITY_H
+
+#include "adj_data_structures.h"
+#include "adj_error_handling.h"
+
+#ifdef HAVE_SLEPC
+#include "slepcsvd.h"
+#endif
+
+typedef struct
+{
+  adj_adjointer* adjointer;
+  adj_variable ic;
+  adj_variable final;
+} adj_svd_data;
+
+typedef struct
+{
+  void* svd_handle;
+} adj_svd;
+
+int adj_compute_tlm_svd(adj_adjointer* adjointer, adj_variable ic, adj_variable final, int nsv, adj_svd* svd_handle, int* ncv);
+int adj_get_svd(adj_svd* svd_handle, int i, adj_scalar* sigma, adj_vector* u, adj_vector* v, adj_scalar* error);
+int adj_destroy_svd(adj_svd* svd_handle);
+
+#endif
+
--- src/adj_error_handling.c
+++ src/adj_error_handling.c
@@ -5,10 +5,10 @@ static int error_check = ADJ_FALSE;
 
 void adj_chkierr_private(int ierr, char* file, int line)
 {
-  char adj_error_codes[10][ADJ_ERROR_MSG_BUF] = {"ADJ_OK", "ADJ_ERR_INVALID_INPUTS", "ADJ_ERR_HASH_FAILED",
+  char adj_error_codes[11][ADJ_ERROR_MSG_BUF] = {"ADJ_OK", "ADJ_ERR_INVALID_INPUTS", "ADJ_ERR_HASH_FAILED",
                                                 "ADJ_ERR_NEED_CALLBACK", "ADJ_ERR_NEED_VALUE", "ADJ_ERR_NOT_IMPLEMENTED",
                                                 "ADJ_ERR_DICT_FAILED", "ADJ_ERR_TOLERANCE_EXCEEDED", "ADJ_ERR_MALLOC_FAILED", 
-                                                "ADJ_ERR_REVOLVE_ERROR"};
+                                                "ADJ_ERR_REVOLVE_ERROR", "ADJ_ERR_SLEPC_ERROR"};
   char adj_warn_codes[4][ADJ_ERROR_MSG_BUF] = {"ADJ_WARN_ALREADY_RECORDED", "ADJ_WARN_COMPARISON_FAILED", "ADJ_WARN_UNINITIALISED_VALUE",
                                                "ADJ_WARN_NOT_IMPLEMENTED"};
 
