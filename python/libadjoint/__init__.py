33a0a04402d257a596309f5be219bbb582be5a31
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2012-09-28 10:56:20 +0100

    Add a header block with __version__ and friends.

--- python/libadjoint/__init__.py
+++ python/libadjoint/__init__.py
@@ -2,4 +2,11 @@
 
 __all__=["clibadjoint_constants","clibadjoint","exceptions","python_utils"]
 
+__version__ = '0.9.2'
+__author__  = 'Patrick Farrell and Simon Funke'
+__credits__ = ['Patrick Farrell', 'Simon Funke', 'David Ham']
+__license__ = 'LGPL-3'
+__maintainer__ = 'Patrick Farrell'
+__email__ = 'patrick.farrell@imperial.ac.uk'
+
 from libadjoint import *
