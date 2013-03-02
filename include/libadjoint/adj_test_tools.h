345aca18defe5f90e14f42bfa2b3cff119998a6e
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2013-03-01 19:27:31 -0500

    More fixes for compiling C with a C++ compiler. Why can't a C++ just compile C without special hacks?

--- include/libadjoint/adj_test_tools.h
+++ include/libadjoint/adj_test_tools.h
@@ -2,8 +2,15 @@
 #define ADJ_TEST_TOOLS_H
 #include "adj_data_structures.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void adj_test_assert(int passed, char *testdesc);
 int adj_sizeof_adjointer(void);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
 
