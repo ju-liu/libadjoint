ebfa8f66be42e997474fa9c6fa9dcb08bf294d69
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2013-03-13 21:46:58 -0400

    More C++ woes

--- include/libadjoint/adj_variable_lookup.h
+++ include/libadjoint/adj_variable_lookup.h
@@ -3,6 +3,10 @@
 
 #include "adj_data_structures.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int adj_find_variable_equation_nb(adj_adjointer* adjointer, adj_variable* var, int* equation_nb);
 
 #ifndef ADJ_HIDE_FROM_USER
@@ -10,6 +14,10 @@ int adj_add_variable_data(adj_variable_hash** hash, adj_variable* var, adj_varia
 int adj_find_variable_data(adj_variable_hash** hash, adj_variable* var, adj_variable_data** data);
 void adj_print_hash(adj_variable_hash** hash);
 int adj_destroy_hash(adj_variable_hash** hash);
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif
