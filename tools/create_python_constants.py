40451f5832e9e9bb96884c2bb8646ac2a3090513
Patrick Farrell <patrick.farrell06@imperial.ac.uk>
2013-10-15 09:30:15 +0100

    No need to pass source path -- we can work that out

--- tools/create_python_constants.py
+++ tools/create_python_constants.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
 import sys
-if len(sys.argv) > 1:
-    source_path = sys.argv[1] + '/'
-else:
-    source_path = ''
+import os.path
+
+source_path = os.path.join(os.path.abspath(os.path.dirname(sys.argv[0])), os.pardir)
 print source_path
 
-if len(sys.argv) > 2:
-    target_path = sys.argv[2] + '/'
+if len(sys.argv) > 1:
+    target_path = sys.argv[1] + '/'
 else:
     target_path = source_path
 print target_path
 
-f = open(source_path + 'include/libadjoint/adj_constants.h', 'r')
+f = open(os.path.join(source_path, 'include', 'libadjoint', 'adj_constants.h'), 'r')
 constants = {}
 
 for line in f:
@@ -27,6 +26,6 @@ f.close()
 
 code = 'adj_constants = %s' % repr(constants)
 
-fc = open(target_path + '/clibadjoint_constants.py', 'w')
+fc = open(os.path.join(target_path, 'clibadjoint_constants.py'), 'w')
 fc.write(code)
 fc.close()
